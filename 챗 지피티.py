599cec83e900ff4a5549a01fcda0bbda20406fca
qkrtjdwn0505 <117055105+qkrtjdwn0505@users.noreply.github.com>
2023-04-08 12:06:26 +0900

    23년 1학기 팀 프로젝트

---
+++ 챗 지피티.py
@@ -0,0 +1,16 @@
+import openai
+
+openai.api_key = "개인 api"
+
+messages = []
+while True:
+    user_content = input("user : ")
+    messages.append({"role": "user", "content": f"{user_content}"})
+
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+
+    assistant_content = completion.choices[0].message["content"].strip()
+
+    messages.append({"role": "assistant", "content": f"{assistant_content}"})
+
+    print(f"GPT : {assistant_content}")
